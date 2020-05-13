74bc724f618a00d612a1465d0c0d226ffa7e0628
ivarog <42422542+ivarog@users.noreply.github.com>
2020-05-13 12:30:45 -0500

    Limpiador y Tokenizador

--- TextManager.py
+++ TextManager.py
@@ -1,59 +1,99 @@
+"""
+	Asignatura: Análisis y procesamiento digital de textos
+	Grupo: 1
+	Semestre: 2020-2
+	
+	Descripción:
+		Programa que a partir de un conjunto de subtitulos limpia el conteniod no necesario para su posterior análisis
+
+    Entradas:
+        Nombre de las carpetas con los subtítulos a limpiar
+        Ejemplo:
+        C:/Users/Ivan/Anaconda3/python.exe c:/Users/Ivan/Documents/Textos/TextManager.py PeliculasBuenas,PeliculasExcelentes,PeliculasMalas,PeliculasRegulares 
+		
+"""
+
 import glob, os
 import sys
 
+"""
+    Find Files
+
+    Recibe: Etension de los archivos a buscar
+    Devuleve: Diccionario
+        Clave: Nombre de la carpeta de los subtitulos
+        Valor: Lista con todos los nombres de los subtítulos en dicha carpeta
+"""
+
 def FindFiles(extension):
-    files = []
-    os.chdir("./"+sys.argv[1])
-    for file in glob.glob("*."+extension):
-        files.append(file)
-    return files
+    filesAllCategories = {}
+    folders = sys.argv[1].split(",")
+    for folder in folders:
+        files = []
+        dirname, filename = os.path.split(os.path.abspath(__file__))
+        os.chdir(dirname+"/"+folder)
+        for file in glob.glob("*."+extension):
+            files.append(file)
+        filesAllCategories[folder] = files
+    return filesAllCategories
 
+"""
+    Process Files
 
-def ProcessFiles(files):
-    for fileName in files:
-        actualFile = open(fileName, encoding="utf8", errors="ignore")
-        linesList = actualFile.readlines()
-        finalList = []
-        for line in linesList:
-            line = line.rstrip()
-            line = line.replace('<i>','')
-            line = line.replace('</i>','')
-            line = line.replace('<b>','')
-            line = line.replace('</b>','')
-            line = line.replace('<font>','')
-            line = line.replace('</font>','')
-            line = line.replace('♪','')
-            line = line.replace('- ','')
-            line = line.replace('"','')
-            line = line.strip()
-            if not line.isdigit() and not '-->' in line:
-                finalList.append(line)
-        finalList = finalList[4:]
-        if not os.path.exists('NewSubs'):
-            os.makedirs('NewSubs')
-        with open('./NewSubs/' + fileName, 'w+', encoding="utf8", errors="ignore") as f:
-            for item in finalList:
-                if item is not '':
-                    f.write("%s\n" % item)
+    Recibe: Diccionario
+        Clave: Nombre de la carpeta de los subtitulos
+        Valor: Lista con todos los nombres de los subtítulos en dicha carpeta
+    Devuleve: Crea un archivo para cada clave del diccionario. Este archivo contiene todos los subtitulos limpios de la categoria.
+"""
+def ProcessFiles(filesAllCategories):
+    dirname, filename = os.path.split(os.path.abspath(__file__))
+    for key in filesAllCategories:
 
-# def ProcessInfo(files):
-#     for fileName in files:
-#         actualFile = open(fileName, encoding="utf8", errors="ignore")
-#         linesList = actualFile.readlines()
-#         finalList = []
-#         for line in linesList:
-#             if "FileName" in line:
-#                 finalList.append(line)
-#         if not os.path.exists('NewSubs'):
-#             os.makedirs('NewSubs')
-#         with open('./NewSubs/' + fileName, 'w+', encoding="utf8", errors="ignore") as f:
-#             for item in finalList:
-#                 if item is not '':
-#                     f.write("%s\n" % item)
+        tokensForCategory = []
+        os.chdir(dirname+"/"+key)
+
+        for fileName in filesAllCategories[key]:
+            actualFile = open(fileName, encoding="utf8", errors="ignore")
+            linesList = actualFile.readlines()
+            linesForMovie = []
+            for line in linesList:
+                line = line.lower()
+                line = line.rstrip()
+                line = line.replace('<i>','')
+                line = line.replace('</i>','')
+                line = line.replace('<b>','')
+                line = line.replace('</b>','')
+                line = line.replace('<font>','')
+                line = line.replace('</font>','')
+                line = line.replace('♪','')
+                line = line.replace('- ','')
+                line = line.replace('"','')
+                line = line.replace('..','')
+                line = line.replace('.','')
+                line = line.replace('\'s','')
+                line = line.replace(',','')
+                line = line.replace('?','')
+                line = line.replace('(','')
+                line = line.replace(')','')
+                line = line.replace('!','')
+                line = line.replace(':','')
+                line = line.replace('1','')
+                line = line.replace('[','')
+                line = line.replace(']','')
+                line = line.replace('\'','')
+                line = line.strip()
+                if not line.isdigit() and not '-->' in line:
+                    linesForMovie.append(line)
+                    for i in line.split():
+                        tokensForCategory.append(i)
+
+            path=dirname + "\\" + key + "Result.txt"
+            with open(path, 'w+', encoding="utf8", errors="ignore") as f:
+                for item in linesForMovie:
+                    if item is not '':
+                        f.write("%s\n" % item)
+
 
 def Main():
     ProcessFiles(FindFiles("srt"))
-    # ProcessInfo(FindFiles("nfo"))
-
-
 Main()
---
+++ Tokenization.py
@@ -0,0 +1,45 @@
+"""
+	Asignatura: Análisis y procesamiento digital de textos
+	Grupo: 1
+	Semestre: 2020-2
+	
+	Descripción:
+		Esta clase realiza la tokenizacion de un conjunto de archivos dados
+    
+    Recibe: El nombre de los archivos a tokenizar
+        Ejemplo
+            C:/Users/Ivan/Anaconda3/python.exe c:/Users/Ivan/Documents/Textos/Tokenization.py PeliculasBuenasResult.txt,PeliculasExcelentesResult.txt,PeliculasMalasResult.txt,PeliculasRegularesResult.txt
+		
+"""
+
+import sys
+import glob, os
+
+
+class Tokenization:
+
+    def __init__(self, files):
+        self.files = files.split(',')
+        self.dirname, self.filename = os.path.split(os.path.abspath(__file__))
+
+    '''
+        CreateTokens
+        Devuelve: Diccionario 
+            Clave: Nombre del archivo a tokenizar
+            Valor: Lista con todos los tokens del archivo
+    '''
+
+    def CreateTokens(self):
+        allTokens = {}
+        for file in self.files:
+            tokens = []
+            actualFile = open(self.dirname+'/'+file, encoding="utf8", errors="ignore")
+            linesList = actualFile.readlines()
+            for line in linesList:
+                for word in line.split():
+                    tokens.append(word)
+            allTokens[file] = tokens
+        return allTokens
+
+tk = Tokenization(sys.argv[1])
+tokens = tk.CreateTokens()
